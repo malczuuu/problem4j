5d840514a5a77882b9d83c1c77d99a467b7a7e0e
Damian Malczewski <damian.m.malczewski@gmail.com>
2018-10-18 19:59:24 +0200

    Add ProblemResponseEntityExceptionHandler

---
+++ problem4j-spring/src/main/java/pl/malczuuu/problem4j/spring/ProblemResponseEntityExceptionHandler.java
@@ -0,0 +1,230 @@
+package pl.malczuuu.problem4j.spring;
+
+import java.time.Instant;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.malczuuu.problem4j.core.Problem;
+import pl.malczuuu.problem4j.core.ProblemBuilder;
+import pl.malczuuu.problem4j.core.ProblemException;
+import pl.malczuuu.problem4j.core.UnauthorizedException;
+
+@SuppressWarnings("NullableProblems")
+public class ProblemResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private final ProblemSupplier problemSupplier;
+
+  public ProblemResponseEntityExceptionHandler() {
+    this(new DefaultProblemSupplier());
+  }
+
+  public ProblemResponseEntityExceptionHandler(ProblemSupplier problemSupplier) {
+    this.problemSupplier = problemSupplier;
+  }
+
+  @ExceptionHandler({ProblemException.class})
+  public ResponseEntity<Object> handleProblemException(ProblemException ex, WebRequest request) {
+    if (ex instanceof UnauthorizedException) {
+      return handleUnauthorizedException((UnauthorizedException) ex, request);
+    }
+    Problem problem = problemSupplier.from(ex).build();
+    HttpStatus status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+  }
+
+  private ResponseEntity<Object> handleUnauthorizedException(
+      UnauthorizedException ex, WebRequest request) {
+    Problem problem = ex.getProblem();
+    HttpStatus status = HttpStatus.valueOf(problem.getStatus());
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("WWW-Authenticate", "Basic realm=\"Basic\", charset=\"UTF-8\"");
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+      HttpRequestMethodNotSupportedException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+      HttpMediaTypeNotSupportedException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
+      HttpMediaTypeNotAcceptableException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingPathVariable(
+      MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(
+      MissingServletRequestParameterException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleServletRequestBindingException(
+      ServletRequestBindingException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleConversionNotSupported(
+      ConversionNotSupportedException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleTypeMismatch(
+      TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(
+      HttpMessageNotReadableException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotWritable(
+      HttpMessageNotWritableException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingServletRequestPart(
+      MissingServletRequestPartException ex,
+      HttpHeaders headers,
+      HttpStatus status,
+      WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleBindException(
+      BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleNoHandlerFoundException(
+      NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleAsyncRequestTimeoutException(
+      AsyncRequestTimeoutException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    Problem problem = problemSupplier.from(ex).build();
+    status = HttpStatus.valueOf(problem.getStatus());
+    return handleExceptionInternal(ex, problem, headers, status, request);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleExceptionInternal(
+      Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+    if (body instanceof Problem) {
+      body = enhanceProblem((Problem) body);
+    }
+    return super.handleExceptionInternal(ex, body, headers, status, request);
+  }
+
+  private Problem enhanceProblem(Problem problem) {
+    ProblemBuilder builder = Problem.builder(problem);
+    if (!problem.hasExtension("timestamp")) {
+      builder.extension("timestamp", Instant.now());
+    }
+    return builder.build();
+  }
+}
